de470b5e2825e648dd483910dd734279ecd204b2
wdfwwkkee <denis.usmanov101@mail.ru>
2023-10-13 12:51:13 +0500

    получение всех кабинетов

---
+++ IVCCRMApi/Controllers/ClassroomController.cs
@@ -0,0 +1,29 @@
+﻿using IVCCRMApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IVCCRMApi.Controllers
+{
+    [ApiController]
+    [Route("classroom")]
+    public class ClassroomContoller : ControllerBase
+    {
+
+        [HttpGet]
+        public void GetList()
+        {
+            IVCContext db = new IVCContext();
+            List <Branch> branches = new List<Branch> ();
+
+            for (int i = 0; i < branches.Count; i++)
+            {
+                branches[i].Name.ToString();
+            }
+        }
+
+        [HttpPost]
+        public void Add(List<Branch> branches)
+        {
+
+        }
+    }
+}
