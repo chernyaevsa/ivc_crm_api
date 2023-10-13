b2a19072cf606df7ebf71befaf8161ec93ec0943
student <student@CLASS-50-08>
2023-10-13 12:40:25 +0500

    Список оборудования /equipment/all GET

---
+++ IVCCRMApi/Controllers/EquipmentController.cs
@@ -0,0 +1,19 @@
+﻿using IVCCRMApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IVCCRMApi.Controllers
+{
+    [ApiController]
+    [Route("equipment")]
+    public class EquipmentController : ControllerBase
+    {
+        [HttpGet]
+        [Route("all")]
+        public ActionResult All()
+        {
+            IVCContext db = new IVCContext();
+            return Ok(db.Equipments);
+        }
+      
+    }
+}
