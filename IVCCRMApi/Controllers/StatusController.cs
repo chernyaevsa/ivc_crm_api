4097e52f419c811aae5864778478f1841a6f7c40
wdfwwkkee <denis.usmanov101@mail.ru>
2023-10-18 15:03:59 +0500

    Revert "Vse good"
    This reverts commit 1663851c690d97a4b94517bc2c32e99db400bfe7.

--- IVCCRMApi/Controllers/StatusController.cs
+++ IVCCRMApi/Controllers/StatusController.cs
@@ -1,28 +1,6 @@
-﻿using IVCCRMApi.Models;
-using Microsoft.AspNetCore.Mvc;
-
-namespace IVCCRMApi.Controllers
+﻿namespace IVCCRMApi.Controllers
 {
-    [ApiController]
-    [Route("equipmentStatus")]
-    public class StatusContoller : ControllerBase
+    public class StatusController
     {
-        [HttpGet]
-        [Route("all")]
-        public IActionResult All()
-        {
-            IVCContext db = new IVCContext();
-            return Ok(db.EquipmentStatuses);
-        }
-
-        [HttpPost]
-        public IActionResult Add(EquipmentStatus equipmentStatus)
-        {
-            IVCContext db = new IVCContext();
-            db.EquipmentStatuses.Add(equipmentStatus);
-            db.SaveChanges();
-            return Ok(equipmentStatus);
-        }
-
     }
 }
