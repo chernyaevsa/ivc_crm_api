42dafd17d00d8f0872d17c06dce8eb3e717fef37
student <student@CLASS-50-08>
2023-10-18 14:35:15 +0500

    Удаление

--- IVCCRMApi/Controllers/EmployeeController.cs
+++ IVCCRMApi/Controllers/EmployeeController.cs
@@ -3,37 +3,36 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace IVCCRMApi.Controllers
 {
-	[ApiController]
-	[Route("employee")]
-	public class EmployeeController : ControllerBase
-	{
-		[HttpGet]
-		[Route("all")]
-		public ActionResult All()
-		{
-			IVCContext db = new IVCContext();
-			return Ok(db.Employees);
-		}
-		[HttpGet]
-		public ActionResult Get(int id)
-		{
-			IVCContext db = new IVCContext();
-			Employee? emp = db.Employees.FirstOrDefault(u => u.Id == id);
-			if (emp == null) return NotFound();
-			return Ok();	
-		}
-
-		[HttpDelete]
+    [ApiController]
+    [Route("employee")]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
         [Route("all")]
-        public ActionResult Delete(int id) 
-		{
-			IVCContext db = new IVCContext();
-			Employee? employee = db.Employees.FirstOrDefault(e => e.Id == id);
-			if (employee == null)
-				return NotFound();
-			db.Employees.Remove(employee);
-			db.SaveChanges();
-			return Ok(employee);
-		}
-	}
+        public ActionResult All()
+        {
+            IVCContext db = new IVCContext();
+            return Ok(db.Employees);
+        }
+        [HttpDelete]
+
+        public ActionResult Delete(int id)
+        {
+            IVCContext db = new IVCContext();
+            Employee? emp = db.Employees.FirstOrDefault(u => u.Id == id);
+            if (emp == null) return NotFound();
+            db.Employees.Remove(emp);
+            db.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Employee employee)
+        {
+            IVCContext db = new IVCContext();
+            db.Employees.Add(employee);
+            db.SaveChanges();
+            return Ok(employee);
+        }
+    }
 }
