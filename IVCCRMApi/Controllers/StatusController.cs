4bdcc659529b315a395bd9b70cb643c70153c132
wdfwwkkee <denis.usmanov101@mail.ru>
2023-10-18 15:07:29 +0500

    asd

--- IVCCRMApi/Controllers/StatusController.cs
+++ IVCCRMApi/Controllers/StatusController.cs
@@ -1,6 +1,28 @@
-﻿namespace IVCCRMApi.Controllers
+﻿using IVCCRMApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IVCCRMApi.Controllers
 {
-    public class StatusController
+    [ApiController]
+    [Route("equipmentStatus")]
+    public class StatusController : ControllerBase
     {
+        [HttpGet]
+        [Route("all")]
+        public IActionResult All()
+        {
+            IVCContext db = new IVCContext();
+            return Ok(db.EquipmentStatuses);
+        }
+
+        [HttpPost]
+        public IActionResult Add(EquipmentStatus equipmentStatus)
+        {
+            IVCContext db = new IVCContext();
+            db.EquipmentStatuses.Add(equipmentStatus);
+            db.SaveChanges();
+            return Ok(equipmentStatus);
+        }
+
     }
 }
