f39f6e7eae973dd37678f7384663d79d9e53ee4e
student <student@CLASS-50-06>
2023-10-19 13:11:21 +0500

    Orders all add

---
+++ IVCCRMApi/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using IVCCRMApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IVCCRMApi.Controllers
+{
+    [ApiController]
+    [Route("order")]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet]
+        [Route("all")]
+        public IActionResult All()
+        {
+            IVCContext db = new IVCContext();
+            return Ok(db.Orders);
+        }
+        [HttpPost]
+        public IActionResult Add(Order order)
+        {
+            IVCContext db = new IVCContext();
+            db.Orders.Add(order);
+            db.SaveChanges();
+            return Ok(order);
+        }
+    }
+}
