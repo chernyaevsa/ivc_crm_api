36546f7a2e6f12f30d31c1adfe87a02a43bc7a93
csa <csa@czsa.ru>
2023-10-26 12:27:15 +0500

    Classroom fix

--- IVCCRMApi/Models/Classroom.cs
+++ IVCCRMApi/Models/Classroom.cs
@@ -5,5 +5,7 @@
 		public int Id { get; set; }
 		public int BranchId { get; set; }
 		public Branch? Branch { get; set; }
+		public string Number { get; set; } = "";
+		public string Name { get; set; } = "";
 	}
 }
